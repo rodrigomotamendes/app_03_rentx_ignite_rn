cd79411df30eb6b8621ed20201ed50f1fb4e1f0f
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-02-11 00:12:05 -0300

    Criação do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,6 +4,8 @@ import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
+import { Car } from '../../components/Car';
+
 import {
   Container,
   Header,
@@ -12,6 +14,26 @@ import {
 } from './styles';
 
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'Ao dia',
+      price: 120,
+    },
+    thumbnail: 'https://www.downloadclipart.net/large/audi-rs5-red-png.png'
+  }
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      period: 'Ao dia',
+      price: 340,
+    },
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png'
+  }
+
   return (
     <Container>
       <StatusBar 
@@ -30,6 +52,10 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne}/>
+      <Car data={carDataTwo}/>
+
     </Container>
   );
 }
