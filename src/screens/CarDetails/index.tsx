3435b75aadaeedec0013bac8f9e6bbff177ac5ee
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-02-16 13:40:44 -0300

    Continuando a interface de Detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -7,6 +7,15 @@ import {
   Container,
   Header,
   CarImage,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
 } from './styles';
 
 export function CarDetails(){
@@ -21,6 +30,24 @@ export function CarDetails(){
           imagesUrl={['https://www.downloadclipart.net/large/audi-rs5-red-png.png']}
         />
       </CarImage>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
 
     </Container>
   );
