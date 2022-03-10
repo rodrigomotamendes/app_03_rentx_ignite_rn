b71f8a897508c6dfedb1eef9ea4fd17f7884b16d
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-03-10 18:31:41 -0300

    resolvendo a marcacão de datas do calendario

--- src/utils/getPlatformDate.ts
+++ src/utils/getPlatformDate.ts
@@ -1,10 +1,5 @@
 import { addDays } from 'date-fns';
-import { Platform } from 'react-native';
 
 export function getPlataformDate(date: Date){
-  if(Platform.OS === 'ios'){
     return addDays(date, 1);
-  } else {
-    return date;
-  }
 }
