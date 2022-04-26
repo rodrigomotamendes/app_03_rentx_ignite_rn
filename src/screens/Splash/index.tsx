e089a7b60683b891d940df233fd14198159f54e3
Rodrigo Mendes <rdrmendes@hotmail.com>
2022-04-26 18:43:22 -0300

    Transições

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button, StyleSheet, Dimensions } from 'react-native';
 
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+import Animated, { 
+  useSharedValue, 
+  useAnimatedStyle,
+  withTiming,
+  Easing
+} from 'react-native-reanimated';
+
+const WIDTH = Dimensions.get('window').width;
 
 import {
   Container
@@ -12,12 +19,19 @@ export function Splash(){
 
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }]
+      transform: [
+        { 
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bezier(.7,-0.01,.08,1.01)
+          }) 
+        }
+      ]
     }
   });
 
   function handleAnimationPosition() {
-    animation.value = Math.random() * 100;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
 
