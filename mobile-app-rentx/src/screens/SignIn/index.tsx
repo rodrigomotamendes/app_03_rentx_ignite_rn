bcaa582c25c389591b45a655d41311c838694735
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-10 18:29:04 -0300

    Autenticação

--- mobile-app-rentx/src/screens/SignIn/index.tsx
+++ mobile-app-rentx/src/screens/SignIn/index.tsx
@@ -11,6 +11,7 @@ import {
  import * as Yup from 'yup';
 
 import theme from '../../styles/theme';
+import { useAuth } from '../../hooks/auth'
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -30,6 +31,7 @@ export function SignIn(){
   const [password, setPassword] = useState('');
 
   const navigation =  useNavigation();
+  const { signIn } = useAuth();
 
   async function handleSignIn() {
     try {
@@ -44,7 +46,7 @@ export function SignIn(){
       await schema.validate({ email, password })
       Alert.alert('Tudo certo')
 
-      //Fazer login
+      signIn({ email, password });
     } catch (error) {
       if(error instanceof Yup.ValidationError) {
         Alert.alert('Opa', error.message);
