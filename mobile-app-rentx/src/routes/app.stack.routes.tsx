a763d5683bec19f21d1ab151b932989481aa7c20
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-10 19:25:30 -0300

    Rotas Privadas e Públicas

--- mobile-app-rentx/src/routes/app.stack.routes.tsx
+++ mobile-app-rentx/src/routes/app.stack.routes.tsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../screens/Home';
@@ -8,33 +8,15 @@ import { SchedulingDetails } from '../screens/SchedulingDetails';
 import { Confirmation } from '../screens/Confirmation';
 import { MyCars } from '../screens/MyCars';
 import { Splash } from '../screens/Splash';
-import { SignIn } from '../screens/SignIn';
-import { SignUpFirstStep } from '../screens/SignUp/SignUpFirstStep';
-import { SignUpSecondStep } from '../screens/SignUp/SignUpSecondStep';
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function StackRoutes(){
+export function AppStackRoutes(){
   return (
     <Navigator headerMode='none' initialRouteName='Home'>
       <Screen 
-        name="SignIn"
-        component={SignIn}
-      />
-      <Screen
-        name="SignUpFirstStep"
-        component={SignUpFirstStep}
-      />
-      <Screen 
-        name="SignUpSecondStep"
-        component={SignUpSecondStep}
-      />
-      <Screen 
         name="Home"
         component={Home}
-        options={{
-          gestureEnabled: false,
-        }}
       />
       <Screen 
         name="CarDetails"
