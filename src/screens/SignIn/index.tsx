c2e842e4bae260b83e1741139c70b81dadd8cb75
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-09 22:19:23 -0300

    Acessibilidade em Inputs

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   StatusBar,
   KeyboardAvoidingView,
@@ -22,6 +22,9 @@ import {
 } from './styles';
 
 export function SignIn(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <KeyboardAvoidingView behavior='position' enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -48,11 +51,15 @@ export function SignIn(){
               keyboardType='email-address'
               autoCorrect={false}
               autoCapitalize="none"
+              onChangeText={setEmail}
+              value={email}
             />
 
             <PasswordInput 
               iconName='lock'
               placeholder='Senha'
+              onChangeText={setPassword}
+              value={password}
             />
           </Form>
 
