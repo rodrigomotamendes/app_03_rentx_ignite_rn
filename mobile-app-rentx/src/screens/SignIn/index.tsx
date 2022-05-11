8eea61cbe9a84043de2805106b0478f6f27920cd
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-11 18:11:03 -0300

    Estrutura da base de dados

--- mobile-app-rentx/src/screens/SignIn/index.tsx
+++ mobile-app-rentx/src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { 
   StatusBar,
@@ -16,6 +16,8 @@ import { useAuth } from '../../hooks/auth'
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+
+import { database } from '../../database';
 
 import {
   Container,
@@ -62,6 +64,16 @@ export function SignIn(){
   function handleNewAccount() {
     navigation.navigate('SignUpFirstStep');
   }
+
+  useEffect(() => {
+    async function loadData() {
+      const userCollection = database.get('users');
+      const users = await userCollection.query().fetch();
+      console.log(users)
+    }
+
+    loadData();
+  }, [])
 
   return (
     <KeyboardAvoidingView behavior='position' enabled>
