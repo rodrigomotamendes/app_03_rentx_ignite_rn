6d24ff5fa406a9cfdbf4bd17a9e96cb05ca2a3f5
Rodrigo Mota Mendes <rdrmendes@hotmail.com>
2022-05-12 15:49:30 -0300

    Sessões do Cadastro

--- mobile-app-rentx/src/screens/Profile/index.tsx
+++ mobile-app-rentx/src/screens/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
@@ -14,9 +14,15 @@ import {
   PhotoContainer,
   Photo,
   PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
 } from './styles';
 
 export function Profile(){
+  const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
+
   const theme = useTheme();
   const navigation = useNavigation();
 
@@ -26,6 +32,10 @@ export function Profile(){
 
   function handleSignOut() {
     
+  }
+
+  function handleOptionChange(optionSelected: 'dataEdit' | 'passwordEdit'){
+    setOption(optionSelected);
   }
 
   return (
@@ -57,6 +67,27 @@ export function Profile(){
           </PhotoButton>
         </PhotoContainer>
       </Header>
+
+      <Content>
+        <Options>
+          <Option 
+            active={option === 'dataEdit'}
+            onPress={() => handleOptionChange('dataEdit')}
+          >
+            <OptionTitle active={option === 'dataEdit'}>
+              Dados
+            </OptionTitle>
+          </Option>
+          <Option 
+            active={option === 'passwordEdit'}
+            onPress={() => handleOptionChange('passwordEdit')}
+          >
+            <OptionTitle active={option === 'passwordEdit'}>
+              Trocar senha
+            </OptionTitle>
+          </Option>
+        </Options>
+      </Content>
 
     </Container>
   );
